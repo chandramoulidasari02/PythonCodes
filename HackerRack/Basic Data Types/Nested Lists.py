c481147afabad8c1f32ae6b644c12e8ad51cbfb9
chandramoulidasari02 <dasari.chandu02@gmail.com>
2025-07-17 10:02:05 +0000

    added more codes

--- HackerRack/Basic Data Types/Nested Lists.py
+++ HackerRack/Basic Data Types/Nested Lists.py
@@ -1,1 +1,17 @@
 # https://www.hackerrank.com/challenges/nested-list/problem
+
+
+if __name__ == '__main__':
+  records = []
+  marks = set()
+  for _ in range(int(input())):
+      name = input()
+      score = float(input())
+      records.append([name,score])
+      marks.add(score)
+  # print(sorted(set(marks))[2])
+  second_lowest = sorted(marks)[1]
+  students = sorted(records)
+  for name,score in students:
+      if second_lowest == score:
+          print(name)
